c4f3a44a69a1ba4641689f8a63915bde14e7c14d
YonePC <YonePC@YONE>
2017-12-22 18:10:08 +0000

    implemented string compare as lambda

--- src/comparatorLambda.java
+++ src/comparatorLambda.java
@@ -7,16 +7,19 @@ public class comparatorLambda {
     public static void main(String[] args) {
 
 
-        Comparator<String> stringComparator = new Comparator<String>() {
+//        Comparator<String> stringComparator = new Comparator<String>() {
+//
+//            @Override
+//            public int compare(String s1, String s2) {
+//                return Integer.compare(s1.length(), s2.length());
+//            }
+//        };
 
-            @Override
-            public int compare(String s1, String s2) {
-                return Integer.compare(s1.length(), s2.length());
-            }
-        };
+        Comparator<String> lambdaComparator = (String s1, String s2) ->
+                Integer.compare(s1.length(), s2.length());
 
         List<String> list = Arrays.asList("********", "**", "*", "");
-        Collections.sort(list, stringComparator);
+        Collections.sort(list, lambdaComparator);
 
         for (String s :
                 list) {

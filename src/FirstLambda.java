26c85052c8e0a9e6963c9d322c3000bfe7c48474
YonePC <YonePC@YONE>
2017-12-22 17:51:19 +0000

    two examples of lambda expressions

---
+++ src/FirstLambda.java
@@ -0,0 +1,35 @@
+
+import java.io.File;
+import java.io.FileFilter;
+
+/**
+ * @author Yone https://app.pluralsight.com/player?course=java-8-whats-new&author=jose-paumard&name=java-8-whats-new-m1&clip=4&mode=live
+ */
+public class FirstLambda {
+
+    public static void main(String[] args) {
+
+//        FileFilter filter = new FileFilter() {
+//
+//            @Override
+//            public boolean accept(File pathname) {
+//
+//                return pathname.getName().endsWith(".java");
+//            }
+//        };
+
+        FileFilter filterNew = (File pathname) ->
+                pathname.getName().endsWith(".txt");
+
+        File dir = new File("..");
+
+        File[] files = dir.listFiles(filterNew);
+
+        for (File f :
+                files)
+
+        {
+            System.out.println(f);
+        }
+    }
+}
---
+++ src/runnableLambda.java
@@ -0,0 +1,31 @@
+public class runnableLambda {
+    public static void main(String[] args) throws InterruptedException {
+        Runnable runnable = () -> {
+            for (int i = 0; i < 3; i++) {
+                System.out.println("Hola soy el hilo" +
+                        Thread.currentThread().getName() +
+                        "|");
+            }
+        };
+
+
+
+        Thread myThread = new Thread(runnable);
+        Thread myThread2 = new Thread(runnable);
+        Thread myThread3 = new Thread(runnable);
+        Thread myThread4 = new Thread(runnable);
+        Thread myThread5 = new Thread(runnable);
+
+        myThread.start();
+        myThread2.start();
+        myThread3.start();
+        myThread4.start();
+        myThread5.start();
+
+        myThread.join();
+        myThread2.join();
+        myThread3.join();
+        myThread4.join();
+        myThread5.join();
+    }
+}

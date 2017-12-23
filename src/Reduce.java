4fe5d8084c2840d6f84452d12affda5c383f0640
YonePC <YonePC@YONE>
2017-12-23 06:43:44 +0000

    computing the max with a reduce and checking that identity argument in a max is not proper, it is better to use an Optional<>

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,14 +1,15 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class Reduce {
     public static void main(String[] args) {
 
         List<Integer> list = Arrays.asList(10, 10, 20);
 
-        Integer target =
+        Optional<Integer> target =
                 list.stream()
-                        .reduce(0, Integer::sum);
+                        .reduce(Integer::max);
 
         System.out.println("El resultado es: " + target);
 

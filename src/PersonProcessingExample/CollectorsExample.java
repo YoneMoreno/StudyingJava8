05a81986d993249c52286f61fbcacc5579e3b85e
YonePC <YonePC@YONE>
2017-12-23 07:09:23 +0000

    learning how to map a file to get input data

---
+++ src/PersonProcessingExample/CollectorsExample.java
@@ -0,0 +1,45 @@
+package PersonProcessingExample;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+
+
+public class CollectorsExample {
+
+    public static void main(String... args) {
+
+        List<Person> persons = new ArrayList<>();
+
+        try (
+                BufferedReader reader =
+                        new BufferedReader(
+                                new InputStreamReader(
+                                        CollectorsExample.class.getResourceAsStream("people.txt")));
+
+                Stream<String> stream = reader.lines()
+        ) {
+
+            stream.map(line -> {
+                String[] s = line.split(" ");
+                Person p = new Person(s[0].trim(), Integer.parseInt(s[1]));
+                persons.add(p);
+                return p;
+            })
+                    .forEach(System.out::println);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
---
+++ src/PersonProcessingExample/Person.java
@@ -0,0 +1,31 @@
+package PersonProcessingExample;
+
+/**
+ *
+ * @author José
+ */
+public class Person {
+
+    private String name;
+    
+    private int age;
+    
+    public Person(){}
+    
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    public int getAge() {
+        return this.age;
+    }
+    
+    public String toString() {
+        return "Person [" + this.name + ", " + this.age + "]";
+    }
+}

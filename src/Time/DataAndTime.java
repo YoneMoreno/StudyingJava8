28c44d64c9ec3cb3a3747cc28ea900f31d415e26
YonePC <YonePC@YONE>
2017-12-23 15:26:21 +0000

    LocalDate from Time API

---
+++ src/Time/DataAndTime.java
@@ -0,0 +1,74 @@
+package Time;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * @author José
+ */
+public class DataAndTime {
+
+    public static void main(String[] args) {
+
+        List<Person> persons = new ArrayList<>();
+
+        readPersonFile(persons);
+
+        LocalDate now = LocalDate.now();
+
+        persons.stream().forEach(
+                p -> {
+                    Period between = Period.between(p.getDateOfBirth(), now);
+                    System.out.println(p.getName() + " nacio hace: " +
+                            between.get(ChronoUnit.YEARS) + " años y " +
+                            between.get(ChronoUnit.DAYS) + " meses " +
+                            "[" + p.getDateOfBirth().until(now, ChronoUnit.MONTHS)
+                            + " meses}"
+                    );
+                }
+        );
+
+
+    }
+
+    private static void readPersonFile(List<Person> persons) {
+        try (
+                BufferedReader reader =
+                        new BufferedReader(
+                                new InputStreamReader(
+                                        DataAndTime.class.getResourceAsStream("people.txt")));
+                Stream<String> stream = reader.lines();
+        ) {
+
+            createPersonFromFileDataAndAddItToList(persons, stream);
+
+        } catch (IOException ioe) {
+            System.out.println(ioe);
+        }
+    }
+
+    private static void createPersonFromFileDataAndAddItToList(List<Person> persons, Stream<String> stream) {
+        stream.map(
+                line -> {
+                    String[] s = line.split(" ");
+                    String name = s[0].trim();
+                    int year = Integer.parseInt(s[1]);
+                    Month month = Month.of(Integer.parseInt(s[2]));
+                    int day = Integer.parseInt(s[3]);
+                    Person p = new Person(name, LocalDate.of(year, month, day));
+                    persons.add(p);
+                    return p;
+                })
+                .forEach(System.out::println);
+    }
+}
---
+++ src/Time/Person.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Time;
+
+import java.time.LocalDate;
+
+/**
+ *
+ * @author José
+ */
+public class Person {
+    
+    private String name;
+    private LocalDate dateOfBirth;
+    
+    public Person(){}
+    
+    public Person(String name, LocalDate dateOfBirth) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + ", dateOfBirth=" + dateOfBirth + '}';
+    }
+}

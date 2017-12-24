ffcca3e58bfdee706fe55892be577c6057333c59
YonePC <YonePC@YONE>
2017-12-24 06:22:17 +0000

    removeIf

--- src/CollectionsAndStreams/Examples.java
+++ src/CollectionsAndStreams/Examples.java
@@ -1,12 +1,33 @@
 package CollectionsAndStreams;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Examples {
     public static void main(String[] args) {
         List<String> strings = getStrings();
         printStrings(strings);
+
+        ArrayList<String> list = getListFrom(strings);
+        removeIfStringLengthIsGreater(2,list);
+        printJoining(list);
+
+    }
+
+    private static void printJoining(ArrayList<String> list) {
+        System.out.println(
+                list.stream().collect(Collectors.joining(", "))
+        );
+    }
+
+    private static void removeIfStringLengthIsGreater(int number,ArrayList<String> list) {
+        boolean b = list.removeIf(s -> s.length() > number);
+    }
+
+    private static ArrayList<String> getListFrom(List<String> strings) {
+        return new ArrayList<>(strings);
     }
 
     private static void printStrings(List<String> strings) {

f11f5ddf32bcc5a5066e0994d0e6bbe167ab4378
YonePC <YonePC@YONE>
2017-12-24 07:35:16 +0000

    implement comparing into Comparator functional interface to generalize comparator's creation

--- src/LiveCode1/Comparator.java
+++ src/LiveCode1/Comparator.java
@@ -1,17 +1,17 @@
 package LiveCode1;
 
 
-
 import java.util.function.Function;
 
 /**
- *
  * @author José Paumard
  */
 @FunctionalInterface
 public interface Comparator<T> {
 
     public int compare(T t1, T t2);
-    
 
+    public static Comparator<Person> comparing(Function<Person, Integer> f) {
+        return (p1, p2) -> f.apply(p1) - f.apply(p2);
+    }
 }
--- src/LiveCode1/MainComparator.java
+++ src/LiveCode1/MainComparator.java
@@ -12,6 +12,13 @@ public class MainComparator {
 
         Comparator<Person> cmpAge = (p1, p2) -> p2.getAge() - p1.getAge();
         Comparator<Person> cmpFirstName = (p1, p2) -> p1.getFirstName().compareTo(p2.getFirstName());
+        Comparator<Person> cmpLastName = (p1, p2) -> p1.getLastName().compareTo(p2.getLastName());
+
+        Function<Person, Integer> fAge = p -> p.getAge();
+        Function<Person, String> fFirstName = p -> p.getFirstName();
+        Function<Person, String> fLastName= p -> p.getLastName();
+
+        Comparator<Person> cmpPerson = Comparator.comparing(fAge);
 
     }
 }

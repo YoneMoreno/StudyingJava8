ccb64460c2a27309c55e778483aab5f749c5ec1b
YonePC <YonePC@YONE>
2017-12-24 07:26:45 +0000

    comparing by name

--- src/LiveCode1/MainComparator.java
+++ src/LiveCode1/MainComparator.java
@@ -11,5 +11,7 @@ public class MainComparator {
     public static void main(String... args) {
 
         Comparator<Person> cmpAge = (p1, p2) -> p2.getAge() - p1.getAge();
+        Comparator<Person> cmpFirstName = (p1, p2) -> p1.getFirstName().compareTo(p2.getFirstName());
 
     }
+}

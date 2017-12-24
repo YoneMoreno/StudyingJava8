05623cd27dccc4eadf29cd72b91b0089cbc3b17f
YonePC <YonePC@YONE>
2017-12-24 05:28:55 +0000

    refactor: extract method

--- src/Strings/StringsEx.java
+++ src/Strings/StringsEx.java
@@ -5,11 +5,22 @@ import java.util.stream.IntStream;
 public class StringsEx {
     public static void main(String[] args) {
 
-        String s = "Hola mundo";
-        IntStream chars = s.chars();
+        String s = getString();
+        IntStream chars = getIntStream(s);
+        applyUpperCase(chars);
+    }
 
-        chars.mapToObj(letter -> (char)letter)
+    private static void applyUpperCase(IntStream chars) {
+        chars.mapToObj(letter -> (char) letter)
                 .map(Character::toUpperCase)
                 .forEach(System.out::print);
+    }
+
+    private static IntStream getIntStream(String s) {
+        return s.chars();
+    }
+
+    private static String getString() {
+        return "Hola mundo";
     }
 }

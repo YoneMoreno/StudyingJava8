a5f4a1219c75e8d71ec713b9d62a07dcdc2e92f3
YonePC <YonePC@YONE>
2017-12-24 05:35:17 +0000

    stringjoiner J8 class

--- src/Strings/StringsEx.java
+++ src/Strings/StringsEx.java
@@ -1,5 +1,6 @@
 package Strings;
 
+import java.util.StringJoiner;
 import java.util.stream.IntStream;
 
 public class StringsEx {
@@ -8,6 +9,13 @@ public class StringsEx {
         String s = getString();
         IntStream chars = getIntStream(s);
         applyUpperCase(chars);
+
+        StringJoiner stringJoiner = new StringJoiner(", ");
+        stringJoiner.add("one").add("two").add("three");
+        String s1 = stringJoiner.toString();
+        System.out.println();
+        System.out.println(s1);
+
     }
 
     private static void applyUpperCase(IntStream chars) {
